d00fd6e480b0d220e04aa76da9793840ea08c038
Kolinjo <kozicmarko@yahoo.com>
2019-04-19 23:48:29 +0200

    Optional, beer on the wall

--- src/optional/BeerOnTheWall.java
+++ src/optional/BeerOnTheWall.java
@@ -8,6 +8,17 @@ package optional;
 
 public class BeerOnTheWall {
 	public static void main(String[] args) {
+		
+		for (int i = 99; i > 0; i--)
+		{
+			if (i > 0)
+			{
+				System.out.println(i + " bottles of beer on the wall, " + i + " bottle of beer. \n Take one down and pass it around," + i + " bottles of beer on the wall.");
+			}
+		}
+		
+		System.out.println(" \n No more bottles of beer on the wall, no more bottles of beer. ");
+		System.out.println("Go to the store and buy some more, 99 bottles of beer on the wall.");
 
 	}
 }
