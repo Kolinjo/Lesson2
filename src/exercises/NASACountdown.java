c63849977f4e977955ed7cf0749abf2a341ebdd0
Kolinjo <kozicmarko@yahoo.com>
2019-04-19 23:36:10 +0200

    Nasa countdown exercise

--- src/exercises/NASACountdown.java
+++ src/exercises/NASACountdown.java
@@ -1,11 +1,33 @@
 package exercises;
 
+import javax.swing.JOptionPane;
+
 /*
  * Count down a rocket launch!
  * 
- * Print the numbers from 10 to 1, then print â€œblastoffâ€. 
+ * Print the numbers from 10 to 1, then print  “blastoff”. 
  */
 
 public class NASACountdown {
 
+	
+	
+	public static void main(String[] args) 
+	{
+	
+		JOptionPane.showMessageDialog(null, "NASA is counting down before a rocket lunch!");
+		
+		for (int i = 10; i >= 0 ; i--) 
+		{
+			if (i > 0){
+				JOptionPane.showMessageDialog(null, "It has left " + i + " seconds to blastoff!!");
+			}else if (i == 0){
+				JOptionPane.showMessageDialog(null, "Rocket is launched!!");
+			}else{
+				JOptionPane.showMessageDialog(null, "Unsucessful rocket launch");
+			}
+			
+		}
+		
+	}
 }
